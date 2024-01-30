d4d6a78784c73675221d5e9e2fd5899b5f84f4cc
Rangga Hussaen <ranggahussaen101@outlook.com>
2024-01-30 07:44:53 +0700

    update mysql

--- Web Smart Lamp/mysql.py
+++ Web Smart Lamp/mysql.py
@@ -1,33 +1,44 @@
 #!/usr/bin/python
 import pymysql
 
-#class dbserver:
-#  def __init__(self, host, user, passwd, database):
-#    self.host = host
-#    self.user = user
-#    self.passwd = passwd
-#    self.database = database
+class DbServer:
+    def __init__(self, host, user, passwd, database):
+        self.host = host
+        self.user = user
+        self.passwd = passwd
+        self.database = database
+        self.connection = None
+        self.cursor = None
 
-#class dbserver:
- #def__init__(self, host, user, passwd, database):
-  #self.host = host
-  #self.user = user
-  #self.passwd = passwd
-  #self.database = database
+    def connect(self):
+        self.connection = pymysql.connect(host=self.host, user=self.user, passwd=self.passwd, database=self.database)
+        self.cursor = self.connection.cursor()
 
-#koneksi = dbserver("192.168.1.2", "test", "test", "smart lamp")
+    def execute_query(self, query):
+        if not self.connection or not self.cursor:
+            raise RuntimeError("Connection is not established. Call connect() first.")
+        self.cursor.execute(query)
+        self.connection.commit()
 
-connection = pymysql.connect(host="localhost", user="root", password="", database="smart_lamp")
-#connection = pymysql.connect(host=koneksi.host, user=koneksi.user, passwd=koneksi.passwd, database=koneksi.database)
-cursor = connection.cursor()
-#query = "use ujicoba;" 
-query = " update users set username = 'admine1', password = 'AndiYG' where id = '1';"
-#query = "update admin_account set username = 'admin', password = '$2y$10$7upvEHlA.BVkUeEVrpNnZOxaBNf3ZHhZSt9LkDh4Lry84eh/BTDSO' where id_admin = '1';"
-#query = "delete from 'admin_account' where 'admin_account'.'id_admin' = 2;"
-cursor.execute(query)
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
-    
-connection.commit()
-connection.close()
+    def close_connection(self):
+        if self.connection:
+            self.connection.close()
+            print("Connection closed.")
+            self.connection = None
+            self.cursor = None
+
+koneksi = DbServer("192.168.1.2", "test", "test", "smart_lamp")
+
+try:
+    koneksi.connect()
+
+    query = "update users set username = 'admin', password = '$2a$12$MWko9Zzg8JfSEePRs9FSXO/nsbWhRfJG2z1jOf0gnOYexAWMCoV6.' where id = '1';"
+    koneksi.execute_query(query)
+
+    print("Update successful!")
+
+except pymysql.Error as e:
+    print("Error: {}".format(e))
+
+finally:
+    koneksi.close_connection()
